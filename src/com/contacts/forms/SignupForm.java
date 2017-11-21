83c833eb8812689b3ac3a8a333d04f5ef0c5b226
saravana2906 <saravana2906@gmail.com>
2017-11-21 11:35:39 +0530

    Initial concrete structure for project

---
+++ src/com/contacts/forms/SignupForm.java
@@ -0,0 +1,41 @@
+package com.contacts.forms;
+
+public class SignupForm {
+	
+	private String username;
+	private String emailid;
+	private String phoneNo;
+	private String password;
+	private String confirmpassword;
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getEmailid() {
+		return emailid;
+	}
+	public void setEmailid(String emailid) {
+		this.emailid = emailid;
+	}
+	public String getPhoneNo() {
+		return phoneNo;
+	}
+	public void setPhoneNo(String phoneNo) {
+		this.phoneNo = phoneNo;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getConfirmpassword() {
+		return confirmpassword;
+	}
+	public void setConfirmpassword(String confirmpassword) {
+		this.confirmpassword = confirmpassword;
+	}
+
+}
